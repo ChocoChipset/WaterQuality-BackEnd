6fa35589a5c9a624d191a9cdfd9569320733c6ae
Jakub Jaroszewski <jakub.jaroszewski@gmail.com>
2012-11-10 12:38:15 +0100

    Add api django app

---
+++ hydroapi/api/resources.py
@@ -0,0 +1,7 @@
+from tastypie import fields
+from tastypie.resources import ModelResource
+
+
+class MeasurementsResource(ModelResource):
+    class Meta:
+        pass
--- hydroapi/hydroapi/urls.py
+++ hydroapi/hydroapi/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import patterns, include, url
+from tastypie.api import Api
+from api.resources import MeasurementsResource
 
 from django.contrib import admin
 admin.autodiscover()
 
+
+v1_api = Api(api_name='v1')
+v1_api.register(MeasurementsResource())
+
+
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include(v1_api.urls)),
 )
