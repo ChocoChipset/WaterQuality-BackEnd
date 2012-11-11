55bcfd3857f740e5594fdb39503929dc881e1875
Jakub Jaroszewski <jakub.jaroszewski@gmail.com>
2012-11-11 13:09:34 +0100

    Improve parsedata command

--- hydroapi/api/management/commands/parsedata.py
+++ hydroapi/api/management/commands/parsedata.py
@@ -8,13 +8,22 @@ class Command(BaseCommand):
     args = ''
     help = 'Runs parsing'
 
+    def run_parsing(self, module_name, file_path):
+        self.stdout.write('Loading %s...' % module_name)
+        parsing_module = imp.load_source(module_name, file_path)
+        self.stdout.write('Running %s.parse()...' % module_name)
+        parsing_module.parse()
+
     def handle(self, *args, **options):
         parsing_path = os.path.join(os.path.dirname(api.__file__), 'parsing')
-        for file_name in os.listdir(parsing_path):
-            if not file_name.startswith('_') and file_name.endswith('.py'):
+        if args:
+            for module_name in args:
+                file_name = module_name + '.py'
                 file_path = os.path.join(parsing_path, file_name)
-                module_name = file_name[:-3]
-                self.stdout.write('Loading %s...' % module_name)
-                parsing_module = imp.load_source(module_name, file_path)
-                self.stdout.write('Running %s.parse()...' % module_name)
-                parsing_module.parse()
+                self.run_parsing(module_name, file_path)
+        else:
+            for file_name in os.listdir(parsing_path):
+                if not file_name.startswith('_') and file_name.endswith('.py'):
+                    file_path = os.path.join(parsing_path, file_name)
+                    module_name = file_name[:-3]
+                    self.run_parsing(module_name, file_path)
